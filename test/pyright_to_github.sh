9decba81dfd93c11b15d554dcd6451fc2a1418a3
Maxime Mulder <maxime.mulder@mcgill.ca>
2024-09-13 18:14:41 -0400

    test

--- test/pyright_to_github.sh
+++ test/pyright_to_github.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # This is a simple script to format Pyright errors (in JSON format) into GitHub errors, used by CI.
 jq -r '.generalDiagnostics[] | "::error file=\(.file),line=\(.range.start.line),col=\(.range.start.character)::\(.message)
